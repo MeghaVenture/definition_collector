bcd6ddad1285913b2a57d95f8cefa57e8f7366be
Meghan <51386873+MeghaVenture@users.noreply.github.com>
2022-12-31 22:45:14 +0100

    Create main.py
    commit - main.py from computer

---
+++ main.py
@@ -0,0 +1,37 @@
+# Definition web scrapper
+# list of websites by how to get words
+browse_urls = ["https://www.merriam-webster.com/",
+        "https://dictionary.cambridge.org/dictionary/english/",
+        "https://dictionary.cambridge.org/dictionary/essential-american-english/",
+        "https://dictionary.cambridge.org/dictionary/essential-british-english/",
+        "https://dictionary.cambridge.org/dictionary/learner-english/",
+        "https://www.dictionary.com/",
+        "https://www.britannica.com/dictionary/eb/browse/a",
+        "https://www.oxfordreference.com/display/10.1093/acref/9780195517965.001.0001/acref-9780195517965;jsessionid=605DBE65211B3625A7B98B1522BC55F5",
+        "https://www.oxfordreference.com/display/10.1093/acref/9780195418163.001.0001/acref-9780195418163",
+        "https://www.oxfordreference.com/display/10.1093/acref/9780195392883.001.0001/acref-9780195392883?rskey=AOs2nh&result=1",
+        "https://www.oxfordreference.com/display/10.1093/acref/9780195584516.001.0001/acref-9780195584516?rskey=AOs2nh&result=5",
+        "https://www.oxfordreference.com/display/10.1093/acref/9780199571123.001.0001/acref-9780199571123?rskey=AOs2nh&result=4"]
+search_urls = ["https://www.ahdictionary.com/",
+               "https://www.ldoceonline.com/"]
+
+# factory that decided on "instructions to use" based on the url_list the url is in
+
+
+        # browse method functions:
+        ## find the next word
+        ## when to stop (no next buttons)
+
+        #search method functions:
+        ## search a word from list of all words from all other dictionaries
+        ## stop when list is complete
+
+# function for saving the word, part of speach, definations, syn, anty, forms,
+        # pronunciation guide in IPA & audio
+        # plus: source name, source url, time_stamp
+
+        ## END OF LINE:  save using word class(s) for app & csv files?
+        ## END/MIDDLE: export a table with the data (pdf or excel)
+
+# log function
+## records act and time stamp
